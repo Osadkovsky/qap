521feef543f6e0fe30ab37943c0810391afa6e5e
Vladimir Osadkovsy <crashoocrash@gmail.com>
2016-12-19 06:10:06 +0200

    pre-alpha scrollbars + starting to making button

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-$('html, body').animate({scrollTop: '+=100'}, 800)
+$(document).ready(function(){
+    $('.checkbox__doc:button').toggle(function(){
+        $('input:checkbox_doc11').attr('checked','checked');
+        $(this).val('uncheck all')
+    },function(){
+        $('input:checkbox_doc11').removeAttr('checked');
+        $(this).val('check all');        
+    })
+})
